3e012d1e32609f7d22341cf118768bbddc8a09a8
Kalpana Yadav <kalpana.yadav@gmail.com>
2017-12-03 17:45:06 -0600

    This is out first commit

---
+++ src/learnTestNG/CommandPrompt.java
@@ -0,0 +1,13 @@
+package learnTestNG;
+
+import org.testng.annotations.Test;
+
+public class CommandPrompt {
+	
+    @Test
+     public void runFromCommand(){
+	   System.out.println("Running from the Command Prompt");
+	
+     }
+     
+}
